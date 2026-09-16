ce54bc2d345baa1a1b7582dd79fc21f48d1555f3
Suhail <suhail@methuselahventures.ai>
2026-09-16 23:06:59 +0530

    Initial files

---
+++ src/main/java/com/example/CrudDtoDemo/CrudDtoDemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.CrudDtoDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudDtoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudDtoDemoApplication.class, args);
+
+
+	}
+
+}
---
+++ src/test/java/com/example/CrudDtoDemo/CrudDtoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CrudDtoDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDtoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
